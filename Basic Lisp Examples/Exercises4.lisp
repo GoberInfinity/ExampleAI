ee26a5d0a1a6f36febd710f835d8b2c9382b1541
Gober <reyesfragosoroberto@gmail.com>
2016-09-03 01:09:52 -0500

    [Added] Changes to package

--- Basic Lisp Examples/Exercises4.lisp
+++ Basic Lisp Examples/Exercises4.lisp
@@ -1,6 +1,6 @@
 ;Reyes Fragoso Roberto
 
-
+;Problema 1
 (defun ultimoElemento (l)
   (car (last l)))
 
@@ -14,3 +14,18 @@
 	    nil))))
 
 (palindromo '(o o s o o)) ; => T
+
+;Problema 
+(defun listRotate (l &key (r nil) (lf nil))
+  (if r (print 'r) (print 'lf)))
+
+(defun rotate (list count)
+  (if (minusp count)
+      (rotate list (+ (length list) count))
+      (nconc (subseq list count) (subseq list 0 count))))
+
+(listRotate '(a b c d e f g h) :r 3)
+
+(subseq "Hola prro" 0 1)
+
+
