ce2dd298898e6bf6b3a06ed8a588f78c8596db50
Gober <reyesfragosoroberto@gmail.com>
2016-09-08 10:05:15 -0500

    [Added] 13th exercise

--- Basic Lisp Examples/Exercises4.lisp
+++ Basic Lisp Examples/Exercises4.lisp
@@ -105,12 +105,51 @@
 (aref "Hola" 0)
 (length "Hola")
 
+;Problema 11
+(defun mmul (A B)
+  (if (not (equal (car (array-dimensions A)) (car (array-dimensions B))))
+      (return-from mmul nil))
+  (let* ((m (car (array-dimensions A)))
+         (n (cadr (array-dimensions A)))
+         (l (cadr (array-dimensions B)))
+         (C (make-array `(,m ,l) :initial-element 0)))
+    (loop for i from 0 to (- m 1) do
+              (loop for k from 0 to (- l 1) do
+                    (setf (aref C i k)
+                          (loop for j from 0 to (- n 1)
+                                sum (* (aref A i j)
+                                       (aref B j k))))))
+    C))
+
+(mmul #2a((1 2) (3 4)) #2a((5 6 7) (8 9 10))) ;=> #2A((21 24 27) (47 54 61))
+
+(length #2a((1 2) (3 4)))
+(equal (array-dimensions #2a((1 2) (3 4))) (array-dimensions #2a((1 2) (1 2))))
+(car (array-dimensions #2a((1 2) (3 4))))
+(car (array-dimensions #2a((-3 -8 3) (-2 1 4))))
+
+;Problema 13 
+(defun recorta (l n)
+  (cond ((equal 0 n) nil)
+	(t (cons (car l) (recorta (cdr l) (- n 1))))))
+
+(defun all-permutations (list)
+  (cond ((null list) nil)
+        ((null (cdr list)) (list list))
+        (t (loop for element in list
+             append (mapcar (lambda (l) (cons element l))
+                            (all-permutations (remove element list)))))))
+
+(defun filtersubset (lista posicion)
+  (all-permutations ( recorta lista posicion)))
+
+(filtersubset '(a b c d) 2) ;=>((A B) (B A))
 
 ;Problema 14
 (defun combinations (count list)
   (cond
     ((zerop count) '(())) 
-    ((endp list)   '())   
+    ((endp list) '())   
     (t (nconc (mapcar (let ((item (first list))) (lambda (combi) (cons item combi)))
                       (combinations (1- count) (rest list)))
               (combinations count (rest list))))))
@@ -127,6 +166,5 @@
 	   
 (If-positive 10) ; => Numero Positivo 
 
-(subseq "Hola prro" 0 1)
 
 
