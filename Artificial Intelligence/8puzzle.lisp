399ae58b6f90fc594e10edf8a7baf550c8df55a6
¨Gober¨ <reyesfragosoroberto@gmail.com>
2016-09-27 22:14:56 -0500

    [Finished] 1st version of the 8puzzle

--- Artificial Intelligence/8puzzle.lisp
+++ Artificial Intelligence/8puzzle.lisp
@@ -376,32 +376,7 @@
                      finally (insertarAFronteraDeBusqueda listaDeDesacomodados metodo 1)))))))
 
 (bestFirstSearch '((2 8 3)(1 4 5)(7 0 6)) '((1 2 3)(8 0 4)(7 6 5)) :Manhattan )
-(bestFirstSearch '((2 8 3)(1 4 5)(7 0 6)) '((1 2 3)(8 0 4)(7 6 5)) :random-value )
+;(bestFirstSearch '((2 8 3)(1 4 5)(7 0 6)) '((1 2 3)(8 0 4)(7 6 5)) :random-value )
 ;(distanciaManhattan '((4 5 7)(6 0 2)(1 3 8)) '((1 2 3) (8 0 4) (7 6 5)))
 (bestFirstSearch '((2 8 3)(1 4 5)(7 0 6)) '((1 2 3)(8 0 4)(7 6 5)) :bestFirstSearch )
-;(bestFirstSearch '((2 8 3)(1 4 5)(7 6 0)) '((1 2 3)(8 4 0)(7 6 5)) :bestFirstSearch )
-
-
-;(trace expandir)
-;(trace bestFirstSearch)
-;(trace numeroDeElementosDesacomodados)
-
-;(numeroDeElementosDesacomodados '((1 2 3)(4 5 6 )(7 8 0)) '((2 1 3)(4 5 6 )(7 8 0)))
-;(operadorIzquierda 0 '((1 2 2)(2 0 3)(7 10 8)))
-;(substitute 'xx 2 '(0 2 3))
-;(operadorValido? :Izquierda '((1 2 3)(4 0 5)(6 7 8)))
-;(aplicarOperador '((:Izquierda nil)) '((1 2 3)(4 0 5)(6 7 8)))
-;(aplicarOperador '((:Derecha nil)) '((1 2 3)(4 0 5)(6 7 8)))
-;(aplicarOperador '((:Arriba nil)) '((1 2 3)(4 0 5)(6 7 8)))
-;(aplicarOperador '((:Abajo nil)) '((1 2 3)(4 0 5)(6 7 8)))
-;(trace expandir)
-;(expandir '((1 2 3)(4 0 5)(6 7 8)))
-;(expandir '((1 0 3)(4 2 5)(6 7 8)))
-;(dondeEstaEspacioEnBlanco '((1 0 3)(4 2 5)(6 7 8)))
-
-(random 5)
-
-
-
-
 
