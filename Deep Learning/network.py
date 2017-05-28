80d38a90b7f72b79ffd872989637f8761504ca62
¨Gober¨ <reyesfragosoroberto@gmail.com>
2017-05-28 11:30:00 -0500

    [Added] Turing Machine

--- Deep Learning/network.py
+++ Deep Learning/network.py
@@ -1,12 +1,72 @@
-class Network(object):
+#### Libraries
+# Standard library
+import random
+import numpy as np
 
-	def __init__(self, sizes):
-		self.num_layers = len(sizes)
-		self.sizes = sizes
+class Network(object):
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
 		#	Network initialization code assumes that the first layer of neurons is an input layer 
 		#	and omits to set any biases for those neurons
-		self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
-		self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
-						
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
+    
+    def feedforward(self, a):
+        """Return the output of the network if "a" is input."""
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a)+b)
+        return a
+
+    def update_mini_batch(self, mini_batch, eta):
+        """Update the network's weights and biases by applying
+        gradient descent using backpropagation to a single mini batch.
+        The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
+        is the learning rate."""
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w-(eta/len(mini_batch))*nw
+                        for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(mini_batch))*nb
+                       for b, nb in zip(self.biases, nabla_b)]
+        
+    def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
+        """Train the neural network using mini-batch stochastic 
+        gradient descent.  The "training_data" is a list of tuples
+        "(x, y)" representing the training inputs and the desired
+         outputs.  The other non-optional parameters are
+         self-explanatory.  If "test_data" is provided then the
+         network will be evaluated against the test data after each
+         epoch, and partial progress printed out.  This is useful for
+         tracking progress, but slows things down substantially."""
+        if test_data: n_test = len(test_data)
+        n = len(training_data)
+        for j in xrange(epochs):
+            random.shuffle(training_data)
+            mini_batches = [
+                training_data[k:k+mini_batch_size]
+                for k in xrange(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+            if test_data:
+                print "Epoch {0}: {1} / {2}".format(j, self.evaluate(test_data), n_test)
+            else:
+                print "Epoch {0} complete".format(j)
+                
+    def evaluate(self, test_data):
+        """Return the number of test inputs for which the neural
+        network outputs the correct result. Note that the neural
+        network's output is assumed to be the index of whichever
+        neuron in the final layer has the highest activation."""
+        test_results = [(np.argmax(self.feedforward(x)), y)
+                        for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)           
+    
 def sigmoid(z):
-	return 1.0/(1.0+np.exp(-z))
+    return 1.0/(1.0+np.exp(-z))    
+# Up to now, we've been discussing neural networks where the output from one 
+# layer is used as input to the next layer. Such networks are called feedforward neural networks. 
