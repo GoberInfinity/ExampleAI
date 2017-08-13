5c959ef7542d4cfa58b400382b208968395b2108
¨Gober¨ <reyesfragosoroberto@gmail.com>
2017-08-12 21:47:55 -0500

    [Added] Merge concept

--- Complexity Analysis/sorts.lisp
+++ Complexity Analysis/sorts.lisp
@@ -25,3 +25,34 @@
 
 (insertion-sort *worst-case*)
 
+(defun merge-sort (A p q r)
+  (let* ((n1 (1+ (- q p)))
+         (n2 (- r q))
+         (ii '1)
+         (jj '1)
+         (LA (make-array (1+ n1)))
+         (RA (make-array (1+ n2))))
+
+    (loop for i from 1 to n1 do
+         (setf (aref LA (1- i)) (aref A (1- (+ p i)))))
+
+    (loop for j from 1 to n2 do
+         (setf (aref RA (1- j)) (aref A (+ q j))))
+
+    (setf (aref LA n1) MOST-POSITIVE-FIXNUM)
+    (setf (aref RA n2) MOST-POSITIVE-FIXNUM)
+
+    (loop for k from p to r do
+         (progn
+           (if (<= (aref LA ii)(aref RA jj))
+               (print "YOlaso")
+               (print "asfds")))
+         )
+
+
+
+    ))
+
+(merge-sort #(9 8 7 6 5 4 3 2 1) '0 '4 '8)
+
+
